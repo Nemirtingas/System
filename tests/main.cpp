31b881bb371a8e5a51273674e15b49a402f878cf
Nemirtingas <nemirtingas@noreply.com>
2023-02-21 10:45:49 +0100

    Add function to get type/variable type as string.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 #include <System/Filesystem.h>
 #include <System/SystemDetector.h>
 #include <System/String.hpp>
+#include <System/TypeName.hpp>
 
 #include <iostream>
 
@@ -12,6 +13,46 @@
 int main(int argc, char *argv[])
 {
     return Catch::Session().run(argc, argv);
+}
+
+class TypeNameTestClass
+{};
+
+struct TypeNameTestStruct
+{};
+
+template<typename T>
+struct XXXX
+{};
+
+TEST_CASE("Type name", "[TypeName]")
+{
+    {
+        TypeNameTestClass  test1, *pointerTest1 = &test1, **pointerTest11 = &pointerTest1, ***pointerTest111 = &pointerTest11;
+        TypeNameTestStruct test2, *pointerTest2 = &test2, **pointerTest22 = &pointerTest2, ***pointerTest222 = &pointerTest22;
+
+        CHECK(System::TypeName::TypeName<TypeNameTestClass>().to_string() == "TypeNameTestClass");
+        CHECK(System::TypeName::TypeName<TypeNameTestClass*>().to_string() == "TypeNameTestClass*");
+        CHECK(System::TypeName::TypeName<TypeNameTestClass**>().to_string() == "TypeNameTestClass**");
+        CHECK(System::TypeName::TypeName<TypeNameTestClass&>().to_string() == "TypeNameTestClass&");
+        CHECK(System::TypeName::TypeName<TypeNameTestClass&&>().to_string() == "TypeNameTestClass&&");
+
+        CHECK(System::TypeName::BaseTypeName(test1).to_string() == "TypeNameTestClass");
+        CHECK(System::TypeName::BaseTypeName(pointerTest1).to_string() == "TypeNameTestClass");
+        CHECK(System::TypeName::BaseTypeName(pointerTest11).to_string() == "TypeNameTestClass");
+        CHECK(System::TypeName::BaseTypeName(pointerTest111).to_string() == "TypeNameTestClass");
+
+        CHECK(System::TypeName::TypeName<TypeNameTestStruct>().to_string() == "TypeNameTestStruct");
+        CHECK(System::TypeName::TypeName<TypeNameTestStruct*>().to_string() == "TypeNameTestStruct*");
+        CHECK(System::TypeName::TypeName<TypeNameTestStruct**>().to_string() == "TypeNameTestStruct**");
+        CHECK(System::TypeName::TypeName<TypeNameTestStruct&>().to_string() == "TypeNameTestStruct&");
+        CHECK(System::TypeName::TypeName<TypeNameTestStruct&&>().to_string() == "TypeNameTestStruct&&");
+
+        CHECK(System::TypeName::BaseTypeName(test2).to_string() == "TypeNameTestStruct");
+        CHECK(System::TypeName::BaseTypeName(pointerTest2).to_string() == "TypeNameTestStruct");
+        CHECK(System::TypeName::BaseTypeName(pointerTest22).to_string() == "TypeNameTestStruct");
+        CHECK(System::TypeName::BaseTypeName(pointerTest222).to_string() == "TypeNameTestStruct");
+    }
 }
 
 TEST_CASE("Load library", "[loadlibrary]")
